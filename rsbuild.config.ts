3e857836683554e2ece6265d203b53dc00b170d5
xxhls <xxhls@foxmail.com>
2025-03-31 01:59:15 +0800

    feat: 初始化react项目

---
+++ rsbuild.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginSass } from '@rsbuild/plugin-sass';
+import { pluginTypedCSSModules } from '@rsbuild/plugin-typed-css-modules';
+
+export default defineConfig({
+  html: {
+    template: "./public/index.html"
+  },
+  plugins: [
+    pluginReact(),
+    pluginSass(),
+    pluginTypedCSSModules()
+  ],
+  tools: {
+    postcss: {
+      postcssOptions: {
+        // eslint-disable-next-line @typescript-eslint/no-require-imports
+        plugins: [require('@tailwindcss/postcss')],
+      },
+    },
+  }
+});
